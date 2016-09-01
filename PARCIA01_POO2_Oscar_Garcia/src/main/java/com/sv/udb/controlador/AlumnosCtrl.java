3cb4fa30a397b91bf56e29758a1c80689545fb75
root <root@oscar-HP-1000-Notebook-PC>
2016-09-01 15:37:00 -0600

    algo

--- PARCIA01_POO2_Oscar_Garcia/src/main/java/com/sv/udb/controlador/AlumnosCtrl.java
+++ PARCIA01_POO2_Oscar_Garcia/src/main/java/com/sv/udb/controlador/AlumnosCtrl.java
@@ -7,10 +7,12 @@
 package com.sv.udb.controlador;
 
 import com.sv.udb.modelo.Alumnos;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /**
  *
@@ -35,6 +37,16 @@ public class AlumnosCtrl {
         em.close();
         emf.close();
         return resp;
+    }
+    
+    public List<Alumnos> consTodo() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("POO2PU");
+        EntityManager em = emf.createEntityManager();
+        Query query = em.createQuery("SELECT l FROM LugaAcce l WHERE l.esta = :esta");
+        List<Alumnos> resultList = query.getResultList();
+        em.close();
+        emf.close();
+        return resultList;
     }
     
 }
